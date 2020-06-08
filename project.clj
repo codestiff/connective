834d9ea767adc29b85106c296640d1ec63eed700
codestiff <64872556+codestiff@users.noreply.github.com>
2020-06-07 23:05:15 -0700

    wip

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
                   ["vcs" "tag" "--no-sign"]
                   ["shell" "git" "branch" "-f" "release"]
                   ["shell" "git" "push" "origin" "release" "-f"]
+                  ["sub" "deploy" "clojars"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["sub" "change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
