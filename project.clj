859e3baacf04a4b7384b35bc3bf4d1b1bb1fd481
codestiff <64872556+codestiff@users.noreply.github.com>
2020-06-07 22:08:28 -0700

    wip

--- project.clj
+++ project.clj
@@ -1,8 +1,20 @@
-(defproject codestiff/connective "0.1.0-SNAPSHOT"
+(defproject codestiff/connective "0.1.0"
   :description "An API for each accessing persisted entities and the respective relationships."
   :url "https://github.com/codestiff/connective"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
 
   :dependencies [[org.clojure/clojure "1.10.1"]]
 
