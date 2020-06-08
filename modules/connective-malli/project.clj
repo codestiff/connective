694411c6080ef0191e7d51d2ac6484342caca4e1
codestiff <64872556+codestiff@users.noreply.github.com>
2020-06-07 23:39:39 -0700

    Version 0.1.7

--- modules/connective-malli/project.clj
+++ modules/connective-malli/project.clj
@@ -1,4 +1,4 @@
-(defproject codestiff/connective.malli "0.1.7-SNAPSHOT"
+(defproject codestiff/connective.malli "0.1.7"
   :description "a connective validator for malli"
   :url "https://github.com/codestiff/connective"
 
