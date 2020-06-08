1be9a5f34cacf64cb4b05a1c6c1de1eced6d4fca
codestiff <64872556+codestiff@users.noreply.github.com>
2020-06-07 22:33:05 -0700

    Version 0.1.3-SNAPSHOT

--- modules/connective-firestore/project.clj
+++ modules/connective-firestore/project.clj
@@ -1,4 +1,4 @@
-(defproject codestiff/connective.firestore "0.1.2"
+(defproject codestiff/connective.firestore "0.1.3-SNAPSHOT"
   :description "A connective adaptor for firestore"
   :url "https://github.com/codestiff/connective"
 
