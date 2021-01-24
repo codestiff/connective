0c8a82d1abc0d200b1c3b41d438d965e22a500ef
codestiff <64872556+codestiff@users.noreply.github.com>
2021-01-23 21:29:34 -0800

    Version 0.1.8

--- modules/connective-malli/project.clj
+++ modules/connective-malli/project.clj
@@ -1,4 +1,4 @@
-(defproject codestiff/connective.malli "0.1.8"
+(defproject codestiff/connective.malli "0.1.9-SNAPSHOT"
   :description "a connective validator for malli"
   :url "https://github.com/codestiff/connective"
 
